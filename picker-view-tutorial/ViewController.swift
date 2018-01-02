293c0f5e187857fdb4d1d1ff2b8de234b3e3ab88
Zephaniah Cohen <zeph.cohen10@gmail.com>
2018-01-02 17:35:05 +0700

    Picker view tutorial

---
+++ picker-view-tutorial/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  picker-view-tutorial
+//
+//  Created by Zephaniah Cohen on 1/2/18.
+//  Copyright © 2018 Zephaniah Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let dataSource = ["Apple", "Microsoft", "Samsung", "Android", "Google"]
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        detailLabel.text = dataSource[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row]
+    }
+}
+
